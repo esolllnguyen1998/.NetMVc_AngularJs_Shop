b1475e3c81b8a4472c5cf6f38b26e739a6185d73
thangdeptrai <leloi9152@gmail.com>
2019-07-26 23:08:12 +0700

    after code-frist model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        [MaxLength(256)]
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        [MaxLength(256)]
+        string UpdateBy { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+        bool Status { get; set; }
+
+    }
+}
