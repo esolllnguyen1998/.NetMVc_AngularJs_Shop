d26f8b1300b648052550995d862d96582917b4cc
thangdeptrai <leloi9152@gmail.com>
2019-07-26 13:30:06 +0700

    Delete About

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -13,12 +13,6 @@ namespace TeduShop.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
 
         public ActionResult Contact()
         {
